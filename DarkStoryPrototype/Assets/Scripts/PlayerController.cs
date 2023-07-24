42941b2a8dab31d1e72eb97b73f00a02cacb0db7
NeuralPrg <chabaneyann@gmail.com>
2023-07-24 15:24:07 +0200

    Update PlayerController.cs

--- DarkStoryPrototype/Assets/Scripts/PlayerController.cs
+++ DarkStoryPrototype/Assets/Scripts/PlayerController.cs
@@ -121,7 +121,7 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        Time.timeScale = 1.5f; // localScale also affects the IEnumerators
+        //Time.timeScale = 1f; // localScale also affects the IEnumerators
 
         if(instance == null)
         {
