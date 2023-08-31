def17a3fc1b469539e70138298123ad286a1245b
NeuralPrg <chabaneyann@gmail.com>
2023-08-31 13:53:15 +0200

    special attack

--- DarkStoryPrototype/Assets/Scripts/BehaviorTree/Actions/Archer/ArcherAttack.cs
+++ DarkStoryPrototype/Assets/Scripts/BehaviorTree/Actions/Archer/ArcherAttack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using Pathfinding;
 using System.Numerics;
 using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 namespace BehaviorDesigner.Runtime.Tasks
 {
@@ -18,6 +19,9 @@ namespace BehaviorDesigner.Runtime.Tasks
         [SerializeField] private UnityEngine.Vector2 normalAttackSize;
         [SerializeField] private Transform specialAttackPos;
         [SerializeField] private UnityEngine.Vector2 specialAttackSize;
+        [SerializeField] private Transform specialAttackBackCheckBox;
+        [SerializeField] private UnityEngine.Vector2 specialAttackBackCheckSize;
+        [SerializeField] private LayerMask cancelSpecialAttackLayers;
         [SerializeField] private int normalAttackDamages = 1;
         [SerializeField] private int specialAttackDamages = 2;
         private bool hasHitPlayer;
@@ -28,7 +32,6 @@ namespace BehaviorDesigner.Runtime.Tasks
         [SerializeField] private SpriteRenderer sr;
         private Rigidbody2D rb;
         private UnityEngine.Vector3 initialLocalSpritePos;
-        private UnityEngine.Vector2 initialLocalCollisionSize;
         private UnityEngine.Vector2 initialLocalCollisionOffset;
 
         #endregion
@@ -43,18 +46,19 @@ namespace BehaviorDesigner.Runtime.Tasks
 
             rb = GetComponent<Rigidbody2D>();
             initialLocalSpritePos = sr.gameObject.transform.localPosition;
-            initialLocalCollisionSize = GetComponent<CapsuleCollider2D>().size;
             initialLocalCollisionOffset = GetComponent<CapsuleCollider2D>().offset; // change the x offset of the collision when the enemy move during the anim
 
             if (!GetComponent<Enemy>().isKnockbacking)
             {
                 if (!isSpecialAttack)
                     StartCoroutine(NormalAttack());
-                //else
+                else if (!Physics2D.OverlapBox(specialAttackBackCheckBox.position, specialAttackBackCheckSize, 0, cancelSpecialAttackLayers))
+                    StartCoroutine(SpecialAttack());
+                else
+                    finished = true;
             }
             else
                 finished = true;
-
         }
 
         public override TaskStatus OnUpdate()
@@ -62,6 +66,7 @@ namespace BehaviorDesigner.Runtime.Tasks
             if (GetComponent<Enemy>().isKnockbacking && Random.Range(0, 2) == 0)
             {
                 sr.gameObject.transform.localPosition = initialLocalSpritePos;
+                GetComponent<CapsuleCollider2D>().offset = initialLocalCollisionOffset;
                 StopAllCoroutines();
                 finished = false;
                 return TaskStatus.Success;
@@ -69,6 +74,8 @@ namespace BehaviorDesigner.Runtime.Tasks
 
             if (finished)
             {
+                sr.gameObject.transform.localPosition = initialLocalSpritePos;
+                GetComponent<CapsuleCollider2D>().offset = initialLocalCollisionOffset;
                 finished = false;
                 return TaskStatus.Success;
             }
@@ -87,6 +94,7 @@ namespace BehaviorDesigner.Runtime.Tasks
             rb.velocity = new UnityEngine.Vector2(0f, rb.velocity.y);
             anim.SetTrigger("Attack");
             sr.gameObject.transform.localPosition = new UnityEngine.Vector3(sr.gameObject.transform.localPosition.x + 0.4f, sr.gameObject.transform.localPosition.y, sr.gameObject.transform.localPosition.z);
+            
             yield return new WaitForSeconds(0.4f / 0.6f);
             DealDamage();
             yield return new WaitForSeconds(0.025f / 0.6f);
@@ -98,17 +106,43 @@ namespace BehaviorDesigner.Runtime.Tasks
             yield return new WaitForSeconds(0.025f / 0.6f);
             DealDamage();
             yield return new WaitForSeconds(0.18f / 0.6f);
+
             anim.SetTrigger("ReturnToIdle");
             sr.gameObject.transform.localPosition = initialLocalSpritePos;
             finished = true;
         }
         private IEnumerator SpecialAttack()
         {
-            rb.velocity = new UnityEngine.Vector2(0f, rb.velocity.y); // vanish, disable collision (layer mask), appear, attack
-            anim.SetTrigger("Attack");
-            yield return new WaitForSeconds(0.06f / 0.6f);
+            hasHitPlayer = false;
+
+            rb.velocity = new UnityEngine.Vector2(0f, rb.velocity.y); // vanish
+            anim.SetTrigger("SpecialAttackVanish");
+            gameObject.layer = LayerMask.NameToLayer("EnemyNoColWithPlayer");
+
+            yield return new WaitForSeconds(0.42f / 0.6f);
+
+            gameObject.layer = LayerMask.NameToLayer("Enemy"); // Shoot
+            gameObject.transform.position = new UnityEngine.Vector3(transform.position.x + (-transform.localScale.x * 1.5f), transform.position.y, transform.position.z);
+            sr.gameObject.transform.localPosition = new UnityEngine.Vector3(sr.gameObject.transform.localPosition.x + 2.3f, sr.gameObject.transform.localPosition.y, sr.gameObject.transform.localPosition.z);
+            anim.SetTrigger("SpecialAttackShoot");
+
+            yield return new WaitForSeconds(0.15f / 0.6f);
             DealDamage();
-            yield return new WaitForSeconds(0.36f / 0.6f);
+            yield return new WaitForSeconds(0.025f / 0.6f);
+            DealDamage();
+            yield return new WaitForSeconds(0.025f / 0.6f);
+            DealDamage();
+            yield return new WaitForSeconds(0.025f / 0.6f);
+            DealDamage();
+            yield return new WaitForSeconds(0.025f / 0.6f);
+            DealDamage();
+            GetComponent<CapsuleCollider2D>().offset -= new UnityEngine.Vector2(0.3f, 0f);
+
+            yield return new WaitForSeconds(0.52f / 0.6f);
+
+            anim.SetTrigger("ReturnToIdle");
+            sr.gameObject.transform.localPosition = initialLocalSpritePos;
+            GetComponent<CapsuleCollider2D>().offset = initialLocalCollisionOffset;
             finished = true;
         }
 
@@ -131,8 +165,11 @@ namespace BehaviorDesigner.Runtime.Tasks
                 var collisionDetected = Physics2D.OverlapBoxAll(specialAttackPos.position, specialAttackSize, 0);
                 foreach (Collider2D attackable in collisionDetected)
                 {
-                    if (LayerMask.LayerToName(attackable.gameObject.layer) == "Player")
+                    if (LayerMask.LayerToName(attackable.gameObject.layer) == "Player" && !hasHitPlayer)
+                    {
                         GetComponent<Enemy>().DealDamageToPlayer(specialAttackDamages);
+                        hasHitPlayer = true;
+                    }
                 }
             }
         }
