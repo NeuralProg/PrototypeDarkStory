76f30e94d2a71dd8c7c44a11456108ccf1c949ae
NeuralPrg <chabaneyann@gmail.com>
2023-09-07 09:35:03 +0200

    parallax

---
+++ DarkStoryPrototype/Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScript : MonoBehaviour
+{
+    private float _startingPos; //This is starting position of the sprites.
+    private float _lengthOfSprite;    //This is the length of the sprites.
+    [SerializeField] private float AmountOfParallax;  //This is amount of parallax scroll. 
+    [SerializeField] private Camera MainCamera;   //Reference of the camera.
+
+
+    void Start()
+    {
+        //Getting the starting X position of sprite.
+        _startingPos = transform.position.x;
+        //Getting the length of the sprites.
+        _lengthOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        Vector3 Position = MainCamera.transform.position;
+        float Temp = Position.x * (1 - AmountOfParallax);
+        float Distance = Position.x * AmountOfParallax;
+
+        Vector3 NewPosition = new Vector3(_startingPos + Distance, transform.position.y, transform.position.z);
+
+        transform.position = NewPosition;
+    }
+}
