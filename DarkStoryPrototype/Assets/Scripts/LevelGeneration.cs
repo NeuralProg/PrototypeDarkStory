cbac9ea2d0116591c7635d4c7af7e1ceabaa596d
NeuralPrg <chabaneyann@gmail.com>
2023-09-16 15:08:46 +0200

    map spawn

--- DarkStoryPrototype/Assets/Scripts/LevelGeneration.cs
+++ DarkStoryPrototype/Assets/Scripts/LevelGeneration.cs
@@ -18,11 +18,20 @@ public class LevelGeneration : MonoBehaviour
     public GameObject player;
     private Vector3 playerSpawnPos;
 
+    public GameObject[] shopLR;
+    public GameObject[] shopLRT;
+    public GameObject[] shopTB;
+    public GameObject[] shopTBL;
+    public GameObject[] shopTBR;
+    private int chancesToSpawnShop = -20;
+    private bool shopHasBeenAdded = false;
+    private int maxShopAmount = 2;
+
     private float moveAmount = 20f;
     private int direction;
 
     private float timeBtwRoom;
-    private float startTimeBtwRoom = 0f;
+    private float startTimeBtwRoom = 0.05f;
 
     private int rightCounter;
 
@@ -69,6 +78,8 @@ public class LevelGeneration : MonoBehaviour
 
     private void Move()
     {
+        bool shouldSpawnShop = (Random.Range(0, 101) < chancesToSpawnShop && maxShopAmount > 0) ;
+
         if( direction == 1 || direction == 2) // Move up
         { 
             if(transform.position.y < maxY)
@@ -78,15 +89,30 @@ public class LevelGeneration : MonoBehaviour
                 Vector2 newPos = new Vector2(transform.position.x, transform.position.y + moveAmount / 2);
                 transform.position = newPos;
 
-                int rand = Random.Range(0, 4);
-                if(rand == 0)
-                    Instantiate(TB[Random.Range(0, TB.Length)], transform.position, Quaternion.identity);
-                else if (rand == 1)
-                    Instantiate(TBL[Random.Range(0, TBL.Length)], transform.position, Quaternion.identity);
-                else if (rand == 2)
-                    Instantiate(TBR[Random.Range(0, TBR.Length)], transform.position, Quaternion.identity);
-                else if (rand == 3)
-                    Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                if (!shouldSpawnShop)
+                {
+                    int rand = Random.Range(0, 4);
+                    if (rand == 0)
+                        Instantiate(TB[Random.Range(0, TB.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 1)
+                        Instantiate(TBL[Random.Range(0, TBL.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 2)
+                        Instantiate(TBR[Random.Range(0, TBR.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 3)
+                        Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    int rand = Random.Range(0, 3);
+                    if (rand == 0)
+                        Instantiate(shopTB[Random.Range(0, shopTB.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 1)
+                        Instantiate(shopTBL[Random.Range(0, shopTBL.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 2)
+                        Instantiate(shopTBR[Random.Range(0, shopTBR.Length)], transform.position, Quaternion.identity);
+
+                    shopHasBeenAdded = true;
+                }
 
                 direction = Random.Range(1, 7);
                 if(direction == 3) 
@@ -112,15 +138,30 @@ public class LevelGeneration : MonoBehaviour
                 Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount / 2);
                 transform.position = newPos;
 
-                int rand = Random.Range(0, 4);
-                if (rand == 0)
-                    Instantiate(TB[Random.Range(0, TB.Length)], transform.position, Quaternion.identity);
-                else if (rand == 1)
-                    Instantiate(TBL[Random.Range(0, TBL.Length)], transform.position, Quaternion.identity);
-                else if (rand == 2)
-                    Instantiate(TBR[Random.Range(0, TBR.Length)], transform.position, Quaternion.identity);
-                else if (rand == 3)
-                    Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                if (!shouldSpawnShop)
+                {
+                    int rand = Random.Range(0, 4);
+                    if (rand == 0)
+                        Instantiate(TB[Random.Range(0, TB.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 1)
+                        Instantiate(TBL[Random.Range(0, TBL.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 2)
+                        Instantiate(TBR[Random.Range(0, TBR.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 3)
+                        Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    int rand = Random.Range(0, 3);
+                    if (rand == 0)
+                        Instantiate(shopTB[Random.Range(0, shopTB.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 1)
+                        Instantiate(shopTBL[Random.Range(0, shopTBL.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 2)
+                        Instantiate(shopTBR[Random.Range(0, shopTBR.Length)], transform.position, Quaternion.identity);
+
+                    shopHasBeenAdded = true;
+                }
 
                 direction = Random.Range(3, 7);
             }
@@ -136,41 +177,73 @@ public class LevelGeneration : MonoBehaviour
             if(transform.position.x < maxX) 
             {
                 Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, roomLayer);
-                if(roomDetection.GetComponent<RoomType>().type != 2 && roomDetection.GetComponent<RoomType>().type != 3 && roomDetection.GetComponent<RoomType>().type != 10)
+                if(roomDetection.GetComponent<RoomType>().type != 2 && roomDetection.GetComponent<RoomType>().type != 3 && roomDetection.GetComponent<RoomType>().type != 4 && roomDetection.GetComponent<RoomType>().type != 5 && roomDetection.GetComponent<RoomType>().type != 10)
                 {
                     if (rightCounter >= 2)
                     {
-                        roomDetection.GetComponent<RoomType>().RoomDestruction();
-                        int randSpawn = Random.Range(0, 3);
-                        if(randSpawn == 0)
-                            Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
-                        else if (randSpawn == 1)
-                            Instantiate(LR[Random.Range(0, LR.Length)], transform.position, Quaternion.identity);
-                        else if (randSpawn == 2)
-                            Instantiate(LRT[Random.Range(0, LRT.Length)], transform.position, Quaternion.identity);
+                        if (!roomDetection.GetComponent<RoomType>().isShop)
+                        {
+                            roomDetection.GetComponent<RoomType>().RoomDestruction();
+                            int randSpawn = Random.Range(0, 3);
+                            if (randSpawn == 0)
+                                Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                            else if (randSpawn == 1)
+                                Instantiate(LR[Random.Range(0, LR.Length)], transform.position, Quaternion.identity);
+                            else if (randSpawn == 2)
+                                Instantiate(LRT[Random.Range(0, LRT.Length)], transform.position, Quaternion.identity);
+                        }
+                        else
+                        {
+                            roomDetection.GetComponent<RoomType>().RoomDestruction();
+                            int randSpawn = Random.Range(0, 2);
+                            if (randSpawn == 0)
+                                Instantiate(shopLR[Random.Range(0, shopLR.Length)], transform.position, Quaternion.identity);
+                            else if (randSpawn == 1)
+                                Instantiate(shopLRT[Random.Range(0, shopLRT.Length)], transform.position, Quaternion.identity);
+
+                            shopHasBeenAdded = true;
+                        }
                     }
                     else
                     {
-                        roomDetection.GetComponent<RoomType>().RoomDestruction();
+                        if (!roomDetection.GetComponent<RoomType>().isShop)
+                        {
+                            roomDetection.GetComponent<RoomType>().RoomDestruction();
+                            int randRightRoom = Random.Range(0, 2);
+                            if (randRightRoom == 0)
+                                Instantiate(TBR[Random.Range(0, TBR.Length)], transform.position, Quaternion.identity);
+                            else if (randRightRoom == 1)
+                                Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                        }
+                        else 
+                        {
+                            roomDetection.GetComponent<RoomType>().RoomDestruction();
+                            Instantiate(shopTBR[Random.Range(0, shopTBR.Length)], transform.position, Quaternion.identity);
 
-                        int randRightRoom = Random.Range(2, 4);
-                        if (randRightRoom == 2)
-                            Instantiate(TBR[Random.Range(0, TBR.Length)], transform.position, Quaternion.identity);
-                        else if (randRightRoom == 3)
-                            Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                            shopHasBeenAdded = true;
+                        }
                     }
                 }
 
                 Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
                 transform.position = newPos;
 
-                int rand = Random.Range(1, 4);
-                if (rand == 1 )
-                    Instantiate(TBL[Random.Range(0, TBL.Length)], transform.position, Quaternion.identity);
-                else if (rand == 3 || rand == 2)
-                    Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                if (!shouldSpawnShop)
+                {
+                    int rand = Random.Range(0, 2);
+                    if (rand == 0)
+                        Instantiate(TBL[Random.Range(0, TBL.Length)], transform.position, Quaternion.identity);
+                    else if (rand == 1)
+                        Instantiate(TBLR[Random.Range(0, TBLR.Length)], transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(shopTBL[Random.Range(0, shopTBL.Length)], transform.position, Quaternion.identity);
+                    shopHasBeenAdded = true;
+                }
 
                 direction = Random.Range(1, 7);
+
             }
             else
             {
@@ -182,8 +255,23 @@ public class LevelGeneration : MonoBehaviour
 
                 Instantiate(roomEnd, transform.position, Quaternion.identity);
                 Instantiate(player, playerSpawnPos, Quaternion.identity);
-                return;
+            }
+
+            if (!shopHasBeenAdded && !shouldSpawnShop)
+            {
+                chancesToSpawnShop += 10;
             }
         }
+
+        if (shouldSpawnShop && !shopHasBeenAdded)
+        {
+            chancesToSpawnShop = 101;
+        }
+        else if (shopHasBeenAdded && chancesToSpawnShop > 0)
+        {
+            chancesToSpawnShop = -30;
+            maxShopAmount --;
+        }
+        shopHasBeenAdded = false;
     }
 }
--- DarkStoryPrototype/Assets/Scripts/RoomType.cs
+++ DarkStoryPrototype/Assets/Scripts/RoomType.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RoomType : MonoBehaviour
 {
     public int type;
+    public bool isShop = false;
 
     public void RoomDestruction()
     {
